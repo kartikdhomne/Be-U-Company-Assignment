40e469f3418d24a41697d0730a6401b5b3d8e4e6
kartikdhomne <92445043+kartikdhomne@users.noreply.github.com>
2022-05-11 17:11:55 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Cardlist from './Components/Cardlist';
 
 function App() {
+  const [value,setValue]=useState('');
+  const [post,setPost]=useState([]);
+
+   useEffect(()=>{
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+    .then(res=>res.json())
+    .then(res=>{
+      setPost(res)})
+  },[])
+  const searchFunction=(search)=>{
+   setPost(post.filter((e)=>{
+      return e['title'].toLowerCase().includes(search.toLowerCase())
+    }))
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input className='searchbox' placeholder='Search here' value={value} onChange={(e)=>{setValue(e.target.value)}}/>
+      <button  className='btn' onClick={()=>searchFunction(value)}>search</button>
+      <Cardlist posts={post}/>
     </div>
   );
 }
